c62f728a105d188d1e1ef8a0583818f51d5815b0
Cate <cpbx29@gmail.com>
2020-11-18 10:17:36 -0500

    FML

--- home.php
+++ home.php
@@ -5,26 +5,19 @@ require('sqlBack.php');
 
 $tweets = getAllTweets();
 ?>
+
 <!DOCTYPE html>
 <html>
-<head>
+<head><style type="text/css">
+        body {
+            font: 20px;
+            background-image: url('https://img.freepik.com/free-photo/blue-square-tiled-texture-background_53876-63563.jpg?size=626&ext=jpg');
 
-    <style type="text/css">
-        table{
-            table-layout:fixed;
-            width: 60%;
         }
-        table tr th:nth-child(3){
-            width: 100%;
-        }
-</style></head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-
+    </style></head>
+<body>
 <h1>Tweets</h1>
 <?php echo get_tweet_html($tweets); ?>
-
-
 
 </body>
 </html>
