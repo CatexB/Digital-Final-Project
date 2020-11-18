1944c98ab483a5ffec311231518f6de66a9b3629
Cate <cpbx29@gmail.com>
2020-11-17 23:30:58 -0500

    God I hate Heroku so god damn much

---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+
+// Check if the user is already logged in, if not, then redirect them to login page
+
+require('connectdb.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head><style type="text/css">
+body {
+ font: 20px; 
+background-image: url('https://img.freepik.com/free-photo/blue-square-tiled-texture-background_53876-63563.jpg?size=626&ext=jpg');
+
+}
+</style></head>
+<body>
+<div class="container">
+
+
+
+<h1>Cookbooks</h1>
+
+
+</div>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <?php
 
-require('connectdb.php');
+session_start();
+ 
+// Check if the user is already logged in, if not, then redirect them to login page
+header("location: /home.php");
 ?>
-
-<!DOCTYPE html>
-<html>
-<?php //echo get_head_and_navbar_html(); ?>
-<head><style type="text/css">
-body {
- font: 20px;
-    background-image: url('https://img.freepik.com/free-photo/blue-square-tiled-texture-background_53876-63563.jpg?size=626&ext=jpg');
 }
-</style></head>
-<body>
-<div class="container">
-
-
-
-<h1>Hello!</h1>
-
-
-</div>
-</body>
-</html>
-
